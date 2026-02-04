453b728c8ff82e4fb316c66df0cc6e828f03b77c
collhoun <2904yr@mail.ru>
2026-02-04 14:17:58 +0300

    Convert model 1 to class.

--- src/model_1.py
+++ src/model_1.py
@@ -6,92 +6,110 @@ import json
 import datetime
 import os
 
-X = read_csv('data/features.csv')
-y = read_csv('data/target.csv').to_numpy().ravel()
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=1)
+class LinearRegressionModel:
+    def __init__(self, eta=0.15, N_iterations=1000) -> None:
+        self.eta = eta
+        self.N_iterations = N_iterations
+        self.w = None
 
-# Масштабирование признаков (обязательно для градиентного спуска)
-scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
+    def model(self, w, X):
+        """
+        Модель для угадывания цен на дома 
 
+        :param w: вектор весов
+        :param X: матрица признаков обучающей выборки
+        """
+        return X @ w
 
-# Добавление столбца единиц для смещения (w0)
-X_train_scaled = np.c_[np.ones(X_train_scaled.shape[0]), X_train_scaled]
-X_test_scaled = np.c_[np.ones(X_test_scaled.shape[0]), X_test_scaled]
+    def loss(self, w, X, y):
+        """
+        Квадратичная функция потерь
 
+        :param w: вектор коэфицентов весов
+        :param X: матрица признаков обучающей выборки
+        :param y: целевые значения
+        """
+        return np.mean((self.model(w, X) - y)**2)
 
-def model(w, X):
-    """
-    Модель для угадывания цен на дома 
+    def gradient(self, w, X, y):
+        """
+        Градиент функции потерь (не совсем понятно как получить градиент в матричном виде, взял у иишки)
 
-    :param w: вектор весов
-    :param X: матрица признаков обучающей выборки
-    """
-    return X @ w
+        :param w: веса модели
+        :param X: матрица признаков
+        :param y: целевые значения
+        """
+        n = X.shape[0]
+        error = self.model(w, X) - y
+        grad = (2 / n) * X.T @ error
+        return grad
 
+    def GD(self):
+        self.X = self.data_reader('./data/features.csv')
+        self.y = self.data_reader(
+            './data/target.csv').to_numpy().ravel()  # type: ignore
+        self.X_train, self.X_test, self.y_train, self.y_test = self.data_train_split(
+            self.X, self.y, 0.2)
+        self.X_train_scaled, self.X_test_scaled = self.data_scaler(
+            self.X_train, self.X_test)
+        if self.w is None:
+            self.w = np.zeros(self.X_train_scaled.shape[1])
+        for _ in range(self.N_iterations):
+            self.w -= self.eta * \
+                self.gradient(self.w, self.X_train_scaled, self.y_train)
 
-def loss(w, X, y):
-    """
-    Квадратичная функция потерь
+        self.model_report(self.X.columns.to_list())  # type: ignore
 
-    :param w: вектор коэфицентов весов
-    :param X: матрица признаков обучающей выборки
-    :param y: целевые значения
-    """
-    return np.mean((model(w, X) - y)**2)
+    def data_reader(self, filename: str):
+        try:
+            return read_csv(filename)
+        except FileNotFoundError:
+            return None
 
+    def data_train_split(self, X, y, test_size: float) -> list:
+        return train_test_split(X, y, test_size=test_size, random_state=1)
 
-def gradient(w, X, y):
-    """
-    Градиент функции потерь (не совсем понятно как получить градиент в матричном виде)
+    def data_scaler(self, X_train, X_test) -> tuple:
+        # Масштабирование признаков (обязательно для градиентного спуска)
+        scaler = StandardScaler()
+        X_train_scaled = scaler.fit_transform(X_train)
+        X_test_scaled = scaler.transform(X_test)
 
-    :param w: веса модели
-    :param X: матрица признаков
-    :param y: целевые значения
-    """
-    n = X.shape[0]
-    error = model(w, X) - y
-    grad = (2 / n) * X.T @ error
-    return grad
+        # Добавление столбца единиц для смещения (w0)
+        X_train_scaled = np.c_[
+            np.ones(X_train_scaled.shape[0]), X_train_scaled]
+        X_test_scaled = np.c_[np.ones(X_test_scaled.shape[0]), X_test_scaled]
+        return X_train_scaled, X_test_scaled
 
+    def model_report(self, columns: list, filename: str = './data/models.json') -> None:
+        if self.w is None:
+            return
+        model_benchmark = {
+            'model': f'{os.path.basename(__file__)}',
+            'omega': self.w[1:].tolist(),
+            'bias': self.w[0],
+            'eta': self.eta,
+            'Number of iterations': self.N_iterations,
+            'feature_names': columns,
+            'trained_on': f'{datetime.datetime.now()}',
+            'mse_on_train': self.loss(self.w, self.X_train_scaled, self.y_train),
+            'mse_on_test': self.loss(self.w, self.X_test_scaled, self.y_test)
+        }
 
-w = np.zeros(X_train_scaled.shape[1])
-eta = 0.15  # шаг обучения
-N = 1000  # количество шагов алгоритма
-# значения коэфициента для вычисления экспоненциального скользящего среднего
-lm = 0.02
-Qe = loss(w, X_train_scaled, y_train)
-
-
-for _ in range(N):
-    Qe = lm * loss(w, X_train_scaled, y_train) + (1 - lm) * Qe
-    w -= eta * gradient(w, X_train_scaled, y_train)
-
-model_benchmark = {
-    'model': f'{os.path.basename(__file__)}',
-    'omega': w[1:].tolist(),
-    'bias': w[0],
-    'eta': eta,
-    'Number of iterations': N,
-    'lm': 0.02,
-    'feature_names': X.columns.to_list(),
-    'trained_on': f'{datetime.datetime.now()}',
-    'mse_on_train': loss(w, X_train_scaled, y_train),
-    'mse_on_test': loss(w, X_test_scaled, y_test)
-}
-
-try:
-    with open('./data/model.json', 'r', encoding='utf-8') as file:
-        data = json.load(file)
-        if len(data) > 1000:
+        try:
+            with open(filename, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                if len(data) > 1000:
+                    data = []
+        except FileNotFoundError:
             data = []
-except FileNotFoundError:
-    data = []
+
+        with open(filename, 'w', encoding='utf-8') as file:
+            data.append(model_benchmark)
+            json.dump(data, file, indent=4)
 
 
-with open('./data/model.json', 'w', encoding='utf-8') as file:
-    data.append(model_benchmark)
-    json.dump(data, file, indent=4)
+if __name__ == '__main__':
+    model = LinearRegressionModel()
+    model.GD()
